97d15f00f679ec81bb5092bf33c4c232bbf8d0e4
Jason Freidman <jason@compnski.com>
2020-09-10 23:36:20 -0700

    Add Arwes theming

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Scene from './Scene'
+import { ThemeProvider, createTheme, Arwes, Puffs } from "arwes";
+
 
 function App() {
-  return (
-    <div className="App">
-        <Scene />
-    </div>
-  );
+    return (
+        <ThemeProvider theme={createTheme()}>
+            <Arwes>
+                <h1>SpaceDuel</h1>
+                <Puffs style={{ width: '100%', height: 1024 }}>
+                    <Scene />
+                </Puffs>                
+            </Arwes>
+        </ThemeProvider>
+    );
 }
+
+
+
 
 export default App;
